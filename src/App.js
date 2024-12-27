d982a91d2a42a6b9307706c30aa6613d3d2287d7
Samuel Gomes <sam2022gs@gmail.com>
2024-12-26 21:06:14 -0300

    Iniciando

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { ToastContainer } from 'react-toastify';
+
+function App() {
+  return (
+    <div  >
+
+      0000000
+      <ToastContainer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/services/firebaseConnection.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC0reSvXMImIysnFbxcmctn3suRYWq-Adk",
+    authDomain: "tasksg-2df08.firebaseapp.com",
+    projectId: "tasksg-2df08",
+    storageBucket: "tasksg-2df08.firebasestorage.app",
+    messagingSenderId: "335658606117",
+    appId: "1:335658606117:web:90e405c0e0f3a1eb45e9cc",
+    measurementId: "G-N4ZPB8YSPB"
+};
+
+const app = initializeApp(firebaseConfig);
